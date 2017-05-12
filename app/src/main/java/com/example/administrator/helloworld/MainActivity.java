cffab4c39e80eaa7197015a117152fd4da612c67
386083520 <386083520@qq.com>
2017-05-12 13:23:00 +0800

    修改了点击事件的写法

--- app/src/main/java/com/example/administrator/helloworld/MainActivity.java
+++ app/src/main/java/com/example/administrator/helloworld/MainActivity.java
@@ -4,16 +4,20 @@ import android.app.Application;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button bt1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bt1= (Button) findViewById(R.id.bt1);
     }
-    public void test(View v){
+
+    @Override
+    public void onClick(View v) {
         Toast.makeText(MainActivity.this,"hhh",Toast.LENGTH_SHORT).show();
     }
 }
