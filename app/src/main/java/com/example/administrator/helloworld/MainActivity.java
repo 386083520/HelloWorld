693817a690e09c00ade37e1378a8eebeb19ca16e
386083520 <386083520@qq.com>
2017-05-12 13:15:15 +0800

    添加了一个点击事件，和一个布局文件

--- app/src/main/java/com/example/administrator/helloworld/MainActivity.java
+++ app/src/main/java/com/example/administrator/helloworld/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.administrator.helloworld;
 
+import android.app.Application;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void test(View v){
+        Toast.makeText(MainActivity.this,"hhh",Toast.LENGTH_SHORT).show();
     }
 }
